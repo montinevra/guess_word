03950a789ed4334c3ffe4b06dc32e131ae59a459
Montinevra <23004878+montinevra@users.noreply.github.com>
2023-10-02 11:26:35 -0700

    Update guess_word.py

--- guess_word.py
+++ guess_word.py
@@ -7,10 +7,9 @@ if not nltk.data.find('corpora/words.zip'):
 	nltk.download("words")
 word_list = words.words()
 
-# Pick a random word. Lowercase only.
-answer = "A"
-while answer != answer.lower():
-	answer = random.choice(word_list)
+# Choose only words that are all lowercase
+word_list_lowers_only = [word for word in word_list if word == word.lower()]
+answer = random.choice(word_list_lowers_only)
 
 guess = ""
 lower = ""
@@ -21,12 +20,10 @@ while guess != answer:
 	guesses += 1
 	if guess == answer:
 		break
-	if answer < guess:
-		if guess < upper or not upper:
+	if answer < guess and (guess < upper or not upper):
 			upper = guess
-	else:
-		if guess > lower or not lower:
-			lower = guess
+	elif guess > lower or not lower:
+		lower = guess
 	if upper and lower:
 		order = f'between "{lower}" and "{upper}"'
 	elif upper:
